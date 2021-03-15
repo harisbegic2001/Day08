980c0e294bd7353f74baee6884924ba80b82957c
Haris Begić <harisbegic01@hotmail.com>
2021-03-15 15:40:03 +0100

    13th task Day08

---
+++ ex13/src/index.js
@@ -0,0 +1,8 @@
+function randomRangeNumber(minNumber, maxNumber){
+
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+}
+console.log(randomRangeNumber(1, 8));
+
+
+module.exports = randomRangeNumber;
